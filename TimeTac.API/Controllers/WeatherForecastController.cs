bc6541668407a607aa61f0fc0d21f900c120e761
lanknai <desilvaswlp@gmail.com>
2023-12-03 11:21:56 +0530

    feat: update

--- TimeTac.API/Controllers/WeatherForecastController.cs
+++ TimeTac.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TimeTac.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Humidity"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
